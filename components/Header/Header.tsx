ebaa41437408aa0dcaf86dc60b420cbf79c2217e
ArturStepanyan10 <stepanyanartur110@gmail.com>
2024-02-20 22:12:27 +0300

    Хеддер

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,25 +2,30 @@ import { HeaderProps } from './Header.props';
 import cn from 'classnames';
 import styles from './Header.module.css';
 import LogoIcon from './logoHosp.svg';
+import LoginIcon from './LoginIcon.svg';
 
 
 export const Header = ({ className, ...props }: HeaderProps): JSX.Element => {
     return (
-
         <header className={cn(className, styles.header)} {...props}>
-            <div>
+            <div className={cn(styles.navbar)}>
                 <a href='../'><LogoIcon className={cn(styles.logo)} /></a>
-
-                <div className={cn(styles.nav)}>
-                    <ul>
-                        <li><a href='#'>Нав1</a></li>
-                        <li><a href='#'>Нав2</a></li>
-                        <li><a href='#'>Нав3</a></li>
-                        <li><a href='#'>Нав4</a></li>
-                    </ul>
+                <ul className={cn(styles.ul)}>
+                    <li>Врачи</li>
+                    <li>Запись на прием</li>
+                    <li>Мед. карта</li>
+                    <li>О нас</li>
+                </ul>
+                <div className={styles.phoneNumbers}>
+                    <p>8 (900) 589-52-17</p>
+                    <p>8 (904) 599-87-15</p>
+                    <p className={styles.freeCall}>Бесплатный звонок по России</p>
                 </div>
+                <a href="../login/" target="_black"><LoginIcon className={styles.login} /></a>
             </div>
         </header>
-
     )
 }
+
+
+
